376a609b6afeb9c98ecfefb9186bf4c66292b818
kritika roy <roykritika00@gmail.com>
2019-05-21 22:50:32 +0530

    bootstrap tab

--- src/App.js
+++ src/App.js
@@ -1,19 +1,3 @@
-// import React from 'react';
-// import SideBar from './sidebar';
-// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-// import './App.css';
-
-// export default function App() {
-//   return (
-//     <div id="App">
-//       <SideBar />
-//       <div id="page-wrap">
-//         <h1>AppDividend</h1>
-//         <h2>Check out our tutorials the side menubar</h2>
-//       </div>
-//     </div>
-//   );
-// }
 import React, { Component } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import SideBar from './sidebar';
@@ -21,6 +5,34 @@ import './App.css';
 import TabContent from './components/TabContent';
 
 class App extends Component {
+  state = {
+    isMJ: false,
+    isJB: false,
+    isDrake: false
+  };
+
+  toggleChangeMJ = () => {
+    this.setState(prevState => ({
+      isMJ: !prevState.isMJ,
+    }));
+  }
+
+  toggleChangeJB = () => {
+    this.setState(prevState => ({
+      isJB: !prevState.isJB,
+    }));
+  }
+
+  toggleChangeDrake = () => {
+    this.setState(prevState => ({
+      isDrake: !prevState.isDrake,
+    }));
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  }
   render() {
     return (
       <div id="App">
@@ -30,6 +42,47 @@ class App extends Component {
             <h2>Check out the side menubar</h2>
            </div>
            <TabContent />
+           <div className="container">
+        <h2>Save the multiple checkbox values in React js</h2>
+        <hr />
+        <form onSubmit = {this.onSubmit}>
+          <div className="form-check">
+            <label className="form-check-label">
+              <input type="checkbox"
+                checked={this.state.isMJ}
+                onChange={this.toggleChangeMJ}
+                className="form-check-input"
+              />
+              MJ
+            </label>
+          </div>
+          <div className="form-check">
+            <label className="form-check-label">
+              <input type="checkbox"
+                checked={this.state.isJB}
+                onChange={this.toggleChangeJB}
+                className="form-check-input"
+              />
+              JB
+            </label>
+          </div>
+          <div className="form-check">
+            <label className="form-check-label">
+              <input type="checkbox"
+                checked={this.state.isDrake}
+                onChange={this.toggleChangeDrake}
+                className="form-check-input"
+              />
+              Drake
+            </label>
+          </div>
+          <div className="form-group">
+            <button className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
       </div>
       
     );
