6ec31e5fd2e9189649fd4daddc7f3544c486ca1a
kritika roy <roykritika00@gmail.com>
2019-05-21 22:43:14 +0530

    bootstrap tab

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import React from 'react';
+// import React from 'react';
+// import SideBar from './sidebar';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import './App.css';
+
+// export default function App() {
+//   return (
+//     <div id="App">
+//       <SideBar />
+//       <div id="page-wrap">
+//         <h1>AppDividend</h1>
+//         <h2>Check out our tutorials the side menubar</h2>
+//       </div>
+//     </div>
+//   );
+// }
+import React, { Component } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import SideBar from './sidebar';
-
 import './App.css';
+import TabContent from './components/TabContent';
 
-export default function App() {
-  return (
-    <div id="App">
-      <SideBar />
-      <div id="page-wrap">
-        <h1>AppDividend</h1>
-        <h2>Check out our tutorials the side menubar</h2>
+class App extends Component {
+  render() {
+    return (
+      <div id="App">
+            <SideBar />
+           <div id="page-wrap">
+            <h1>AppDividend</h1>
+            <h2>Check out our tutorials the side menubar</h2>
+           </div>
+           <TabContent />
       </div>
-    </div>
-  );
+      
+    );
+  }
 }
+
+export default App;
